02f958cebc7ca008ee64c0ee1d36c5c0f98d8ed5
Dale Thompson <dale.thompson@vastec.com>
2018-11-28 15:23:47 -0500

    Added observer pattern!

---
+++ DesignPatternsInJavascript/Observer/main.js
@@ -0,0 +1,109 @@
+var Task = require('./task');
+
+var auditingService = function() {
+  var message = 'Auditing ';
+  this.update = function(task) {
+    console.log(message + task.user + ' for task ' + task.name);
+  };
+};
+
+var loggingService = function() {
+  var message = 'Logging ';
+  this.update = function(task) {
+    console.log(message + task.user + ' for task ' + task.name);
+  };
+};
+
+var notificationService = function() {
+  var message = 'Notifying ';
+  this.update = function(task) {
+    console.log(message + task.user + ' for task ' + task.name);
+  };
+};
+
+
+var ObserverList = function () {
+  this.observerList = [];
+};
+
+ObserverList.prototype.add = function(obj) {
+  return this.observerList.push(obj);
+};
+
+ObserverList.prototype.get = function(index) {
+  if (index > -1 && index < this.observerList.length) {
+    return this.observerList[index];
+  }
+};
+
+ObserverList.prototype.count = function() {
+  return this.observerList.length;
+};
+
+ObserverList.prototype.removeAt = function(index) {
+  this.observerList.splice(index, 1);
+};
+
+ObserverList.prototype.indexOf = function (obj, startIndex) {
+  var i = startIndex;
+
+  while (i < this.observerList.length) {
+    if (this.observerList[i] === obj) {
+      return i;
+    }
+    i++;
+  }
+
+  return -1;
+};
+
+
+
+
+var ObservableTask = function(data) {
+  Task.call(this, data);
+  this.observers = new ObserverList();
+};
+
+ObservableTask.prototype.addObserver = function(observer) {
+  this.observers.add(observer);
+};
+
+ObservableTask.prototype.removeObserver = function(observer) {
+  this.observers.removeAt(this.observers.indexOf(observer, 0));
+};
+
+ObservableTask.prototype.notify = function(context) {
+  var observerCount = this.observers.count();
+  for (var i = 0; i < observerCount; i++){
+    this.observers.get(i)(context); // <-- get and call
+  }
+};
+
+// override save
+ObservableTask.prototype.save = function() {
+  this.notify(this);
+
+  Task.prototype.save.call(this);
+};
+
+
+
+//var task1 = new Task({ name: 'create a demo for constructors', user: 'Jon' });
+var task1 = new ObservableTask({ name: 'create a demo for constructors', user: 'Jon' });
+
+var audit = new auditingService();
+var log = new loggingService();
+var notify = new notificationService();
+
+
+task1.addObserver(notify.update);
+task1.addObserver(audit.update);
+task1.addObserver(log.update);
+
+task1.save();
+
+
+task1.removeObserver(audit.update);
+
+task1.save();
